bbddb7299d642b1e2051f6d40bfe56ee0da049bd
Andreas Sundqvist <andreas@stefna.is>
2021-05-18 14:03:48 +0200

    Make ActionDispatcher required in actions
    This is so that the di container properly reads the container

--- src/Common/Actions/AbstractAction.php
+++ src/Common/Actions/AbstractAction.php
@@ -4,9 +4,20 @@ namespace Circli\ServerlessCore\Common\Actions;
 
 use Circli\WebCore\Common\Input\RawInput;
 use Circli\WebCore\Common\Responder\ApiResponder;
+use Polus\Adr\Interfaces\ActionDispatcher;
+use Polus\Adr\Interfaces\Resolver;
+use Psr\Http\Message\ResponseFactoryInterface;
 
 abstract class AbstractAction extends \Polus\Adr\Bref\Actions\AbstractAction
 {
+    public function __construct(
+        Resolver $resolver,
+        ResponseFactoryInterface $responseFactory,
+        ActionDispatcher $actionDispatcher,
+    ) {
+        parent::__construct($resolver, $responseFactory, $actionDispatcher);
+    }
+
     protected ?string $input = RawInput::class;
     protected ?string $responder = ApiResponder::class;
 }
--- src/Common/Actions/AbstractDomainAction.php
+++ src/Common/Actions/AbstractDomainAction.php
@@ -3,8 +3,19 @@
 namespace Circli\ServerlessCore\Common\Actions;
 
 use Circli\WebCore\Common\Responder\ApiResponder;
+use Polus\Adr\Interfaces\ActionDispatcher;
+use Polus\Adr\Interfaces\Resolver;
+use Psr\Http\Message\ResponseFactoryInterface;
 
 abstract class AbstractDomainAction extends \Polus\Adr\Bref\Actions\AbstractDomainAction
 {
+    public function __construct(
+        Resolver $resolver,
+        ResponseFactoryInterface $responseFactory,
+        ActionDispatcher $actionDispatcher,
+    ) {
+        parent::__construct($resolver, $responseFactory, $actionDispatcher);
+    }
+
     protected ?string $responder = ApiResponder::class;
 }
